d6a166e0a864dc9afdce81dd30116464a5fefe58
Brinda <brindatreesabiju10@gmail.com>
2023-03-16 10:33:52 +0530

    first code changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MembershipComponent } from './pages/membership/membership.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { SolarsystemComponent } from './pages/solarsystem/solarsystem.component';
+import { SingleComponent } from './single/single.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'' ,component:HomepageComponent
+  },
+  {
+    path:'about' ,component:AboutComponent
+  },
+  {
+    path:'planets' ,component:PlanetsComponent
+  },
+  {
+    path:'single' ,component:SingleComponent
+  },
+  {
+    path:'solarsystem' ,component:SolarsystemComponent
+  },
+  {
+    path:'membership' ,component:MembershipComponent
+  }
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { FooterComponent } from './utils/footer/footer.component';
+import { SingleComponent } from './single/single.component';
+import { FormsModule } from '@angular/forms';
+import { SolarsystemComponent } from './pages/solarsystem/solarsystem.component';
+import { MembershipComponent } from './pages/membership/membership.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    PlanetsComponent,
+    NavbarComponent,
+    FooterComponent,
+    SingleComponent,
+    SolarsystemComponent,
+    MembershipComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
